bf16a40f69d1fd4f9cd80eb589078fbb9abd9d3b
ctzhenghua <ctzhenghua@163.com>
2017-07-25 07:28:38 -0700

    Checkout New Dev Branch

--- C++/Basic/Virtual/TestVirtual.cc
+++ C++/Basic/Virtual/TestVirtual.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
+
+using Func= void(*)();
 class super{
 	public:
 		super(int i):a(i){cout <<"constructor -I am super!\n";}
 		virtual ~super(){cout << "DeCon- I am super\n";}
 		virtual void someMethod(){cout <<"Hello Super!\n";}
+		virtual void someMethod2(){cout <<"Hello Super 2!\n";}
 	private:
 		int a;
 };
@@ -14,20 +17,37 @@ class sub: public super{
 		sub():super(1){cout <<"constructor -I am Sub!\n";}
 		virtual ~sub(){cout << "DeCon- I am Sub\n";}
 		virtual void someMethod()override{cout <<"hello Sub\n";}
+		virtual void someMethod2(){cout <<"hello Sub 2!\n";}
 		virtual void otherMethod(){cout <<"sub other\n";}
 };
 
-int main()
+
+void testConstructorAndDymamic()
 {
+	cout <<R"(After--------- super *test = new sub(); the out will be:)" <<endl;
 	super *test = new sub();
 
-	cout << "using super pointer to use override method:" ;
+	cout << "using super pointer to use override method:" <<endl;
 	test->someMethod();
 
 	//this can not compile
-	//cout << "using super pointer to use new sub method :" ;
+	//cout << "using super pointer to use new sub method :" <<endl;
 	//test -> otherMethod();
 
 	delete test;
+}
+
+void testMemoryOfVirtual()
+{
+	super b(0);
+	Func pfun = nullptr;
+	cout << "virtual Table Address:" << (int*)(&b) <<endl;
+	cout << "virtual Table First Func Address:" << (int*) *(int*)(&b) <<endl;
+	pfun = (Func) *((int*) *(int*)(&b));
+	pfun();
+}
+int main()
+{
+	testMemoryOfVirtual();
 	return 0;
 }
